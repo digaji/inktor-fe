fefd6fcf999b909885f8e33fbebb3abffa2d2de4
Bryn Ghiffar <bryn.ghiffar@gmail.com>
2024-05-03 11:02:44 +0700

    created new landing page

--- src/components/templates/Home/index.tsx
+++ src/components/templates/Home/index.tsx
@@ -1,8 +1,6 @@
+import { Button, Input, Space } from 'antd'
 import { FC, useCallback, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
-
-import Button from '@/components/atoms/Button'
-import Input from '@/components/atoms/Input'
 
 const Home: FC = () => {
   const [roomId, setRoomId] = useState('')
@@ -25,33 +23,52 @@ const Home: FC = () => {
     navigate(roomId)
   }
 
+  const handleJoinRoom = () => {
+    navigate(roomId)
+  }
+
   return (
-    <main className='mx-auto mt-6 flex flex-col items-center gap-6'>
-      <h1 className='text-8xl font-bold tracking-wide'>Inktor</h1>
-
-      <div className='flex flex-col items-center gap-4'>
-        <Button
-          text='Create'
-          textClassName='text-4xl'
-          onClick={() => handleCreateRoom()}
-        />
-
-        <div className='flex items-center gap-4'>
-          <p className='whitespace-nowrap text-4xl font-bold'>Room ID:</p>
-
-          <Input
-            id='roomId'
-            type='text'
-            placeholder={'Enter Room ID'}
-            value={roomId}
-            onChange={handleChangeRoomId}
+    <main className='flex h-screen items-center justify-center bg-[linear-gradient(to_right,grey_1px,transparent_1px),linear-gradient(to_bottom,grey_1px,transparent_1px)] bg-[center_top_1rem] py-10 [background-size:40px_40px]'>
+      <div className='relative h-[500px] w-[450px] rounded-xl bg-white drop-shadow-2xl'>
+        <div className='absolute left-[50%] h-32 w-32 translate-x-[-50%] translate-y-[-50%] drop-shadow-xl'>
+          <img
+            src='/logo.svg'
+            alt='inktor logo'
           />
-
-          <Button
-            text='Join'
-            href={`/${roomId}`}
-            textClassName='text-4xl'
-          />
+        </div>
+        <div className='flex h-full flex-col pt-20'>
+          <div className='flex h-12 justify-center'>
+            <img
+              src='/inktorwritinglogo.svg'
+              alt='inktor logo'
+            />
+          </div>
+          <div className='flex flex-grow flex-col gap-3 px-9 pt-20'>
+            <Space.Compact className='w-full'>
+              <Input
+                placeholder='Room ID'
+                value={roomId}
+                onChange={handleChangeRoomId}
+              />
+              <Button
+                onClick={handleJoinRoom}
+                disabled={roomId === ''}
+              >
+                Join Room
+              </Button>
+            </Space.Compact>
+            <div className='flex items-center gap-2'>
+              <div className='flex-grow border-t border-gray-400' />
+              <span className='text-gray-400'>or</span>
+              <div className='flex-grow border-t border-gray-400' />
+            </div>
+            <Button
+              className='w-full'
+              onClick={handleCreateRoom}
+            >
+              Create Room
+            </Button>
+          </div>
         </div>
       </div>
     </main>
