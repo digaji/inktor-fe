ce3746c8ed504c2a6c83bdeaad4fe2f6f06c22c0
Bryn Ghiffar <bryn.ghiffar@gmail.com>
2024-06-09 19:52:20 +0700

    remove strict mode import

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import './index.css'
 
-import React from 'react'
+// import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
